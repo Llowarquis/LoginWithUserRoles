cfc979aa718459113ab7b78d472bef3ba77febb4
Yohualkis <Yohualkis99@gmail.com>
2024-11-07 22:53:43 -0400

    Se  creo el modelo LoginViewModel

---
+++ LoginWithUserRoles/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LoginWithUserRoles.Models.ViewModels;
+
+public class LoginViewModel
+{
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, Introduzca el Nombre de Usuario")]
+    public string? Username { get; set; }
+
+
+    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, Introduzca la Contraseña")]
+    public string? Password { get; set; }
+}
