4a5160761558eec07f154125a30bea4e251b6c93
Yohualkis <Yohualkis99@gmail.com>
2024-11-07 22:52:54 -0400

    Se creo el contexto de la app

---
+++ LoginWithUserRoles/Context/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using LoginWithUserRoles.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginWithUserRoles.Context;
+
+public class Contexto : DbContext
+{
+	public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+	public DbSet<Usuarios> Usuarios { get; set; }
+}
