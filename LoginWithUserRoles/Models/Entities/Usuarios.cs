dd3b99f0653d714e51f41de251496a4041c03694
Yohualkis <Yohualkis99@gmail.com>
2024-11-07 22:53:20 -0400

    Se creo la entidad Usuarios

---
+++ LoginWithUserRoles/Models/Entities/Usuarios.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LoginWithUserRoles.Models.Entities;
+
+public class Usuarios
+{
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+	public int UsuarioId { get; set; }
+
+
+	[MaxLength(25)]
+    public string? Username { get; set; }
+
+
+	[MaxLength(64)]
+    public string? Password { get; set; }
+
+
+    [MaxLength(20)]
+    public string? Rol { get; set; }
+}
